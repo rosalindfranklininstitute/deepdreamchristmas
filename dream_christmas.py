b1471ea8f07d4169cfa3be7b98d29e270c093ddc
pje39613 <joss.whittle@rfi.ac.uk>
2021-12-03 06:42:22 +0000

    Add some dream arguments

--- dream_christmas.py
+++ dream_christmas.py
@@ -7,12 +7,18 @@ from style_transfer.utils.img import tensor_to_image, image_loader, resize2small
 import argparse
 
 parser = argparse.ArgumentParser(description="Generate Style-Transfer image and apply DeepDream to produce GIF")
-parser.add_argument("--size", type=int, default=512, help="Resolution")
-parser.add_argument("-s", "--style", required=True, help="File path to style image")
-parser.add_argument("-c", "--content", required=True, help="File path to content image")
-parser.add_argument("-i", "--iterations", type=int, default=100, help="Style transfer iterations")
-parser.add_argument("-n", "--noise", type=float, default=0.1, help="Magnitude of noise to add for style transfer")
-parser.add_argument("-o", "--output", required=True, help="Output file name for the deep dream gif")
+
+parser.add_argument("--style", required=True, help="File path to style image")
+parser.add_argument("--content", required=True, help="File path to content image")
+parser.add_argument("--style_iter", type=int, default=100, help="Style transfer iterations")
+parser.add_argument("--style_noise", type=float, default=0.1, help="Magnitude of noise to add for style transfer")
+
+parser.add_argument("--dream_iter", type=int, default=10, help="Dream per frame gradient ascent iterations")
+parser.add_argument("--dream_layers", type=str, nargs='+', help="Layer whose activations we should maximize while dreaming", default=['relu4_3'])
+
+parser.add_argument("--size", type=int, default=512, help="Output GIF resolution")
+parser.add_argument("--fps", type=int, default=10, help="Output GIF framerate")
+parser.add_argument("--output", default="output.gif", help="Output GIF filename")
 
 args = parser.parse_args()
 
@@ -24,11 +30,10 @@ style_img = image_loader(style_img, args.size, device)
 content_img = image_loader(content_img, args.size, device)
 
 def style_transfer(device, style_img, content_img, args):
-
     # Seed image is content image with some optional noise
-    input_img = content_img.clone() + (torch.randn(content_img.data.size(), device=device) * args.noise)
-
-    return Generator().run_style_transfer(content_img, style_img, input_img, num_steps=args.iterations)
+    input_img = content_img.clone() + (torch.randn(content_img.data.size(), device=device) * args.style_noise)
+    # Apply style transfer
+    return Generator().run_style_transfer(content_img, style_img, input_img, num_steps=args.style_iter)
 
 def deep_dream(device, seed_image, args):
     pass
