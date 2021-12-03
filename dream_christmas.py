15f3d4672d92acebac2999eaf92056a985a1b132
pje39613 <joss.whittle@rfi.ac.uk>
2021-12-03 05:43:43 +0000

    Update main script in prep for deep dream

--- dream_christmas.py
+++ dream_christmas.py
@@ -1,50 +1,33 @@
 import torch
-from model.generator import Generator
-from utils.img import imshow, image_loader, resize2smallest
 import matplotlib.pyplot as plt
+
+from style_transfer.model.generator import Generator
+from style_transfer.utils.img import imshow, image_loader, resize2smallest
+
 import argparse
 
-
-def __option_parser():
-    """
-    Argument Parser
-    Returns
-    -------
-    """
-    parser = argparse.ArgumentParser(description="Generate StyleGan Image")
-    parser.add_argument(
-        "-s", "--style_image", required=True, help="File path to style image"
-    )
-    parser.add_argument(
-        "-c", "--content_image", required=True, help="File path to content image"
-    )
-    parser.add_argument(
-        "-i",
-        "--iterations",
-        type=int,
-        default=300,
-        help="Amount of iterations doing style transfer",
-    )
-    parser.add_argument(
-        "-n", "--noise", action="store_true", help="Add noise, image will look crazy"
-    )
-
-    args = parser.parse_args()
-    return args
+parser = argparse.ArgumentParser(description="Generate Style-Transfer image and apply DeepDream to produce GIF")
+parser.add_argument("--size", type=int, default=512, help="Resolution")
+parser.add_argument("-s", "--style", required=True, help="File path to style image")
+parser.add_argument("-c", "--content", required=True, help="File path to content image")
+parser.add_argument("-i", "--iterations", type=int, default=100, help="Style transfer iterations")
+parser.add_argument("-n", "--noise", type=float, default=0.1, help="Magnitude of noise to add for style transfer")
 
 
-def main():
+args = parser.parse_args()
+
+
+def style_transfer(args):
+
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    imsize = 512 if torch.cuda.is_available() else 128
-    args = __option_parser()
-    style_img, content_img = resize2smallest(args.style_image, args.content_image)
 
-    style_img = image_loader(style_img, imsize, device)
-    content_img = image_loader(content_img, imsize, device)
-    if args.noise:
-        input_img = torch.randn(content_img.data.size(), device=device)
-    else:
-        input_img = content_img.clone()
+    # Load style and content images, resize them to the same size, then resize them both to the target resolution
+    style_img, content_img = resize2smallest(args.style, args.content)
+    style_img   = image_loader(style_img, args.size, device)
+    content_img = image_loader(content_img, args.size, device)
+
+    # Seed image is content image with some optional noise
+    input_img = content_img.clone() + (torch.randn(content_img.data.size(), device=device) * args.noise)
 
     plt.figure()
     imshow(content_img, title="Content Image")
@@ -52,11 +35,7 @@ def main():
     plt.figure()
     imshow(style_img, title="Style Image")
 
-    gen = Generator()
-
-    output = gen.run_style_transfer(
-        content_img, style_img, input_img, num_steps=args.iterations
-    )
+    output = Generator().run_style_transfer(content_img, style_img, input_img, num_steps=args.iterations)
 
     plt.figure()
     imshow(output, title="Output Image")
@@ -64,6 +43,4 @@ def main():
     plt.ioff()
     plt.show()
 
-
-if __name__ == "__main__":
-    main()
+style_transfer(args)
