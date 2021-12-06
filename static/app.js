78bbf2af7b6e2c3da45bd90536923e6656201041
pje39613 <joss.whittle@rfi.ac.uk>
2021-12-06 12:51:29 +0000

    Adjust modal

--- static/app.js
+++ static/app.js
@@ -38,27 +38,6 @@ class HostApp extends React.Component {
                         </header>
                         <div className="container">
 
-                            <div className="modal fade" id="uploadModal" tabIndex="-1" role="dialog"
-                             aria-labelledby="uploadModalLabel" aria-hidden="true">
-                                <div className="modal-dialog" role="document">
-                                    <div className="modal-content">
-                                        <div className="modal-header">
-                                            <h5 className="modal-title" id="uploadModalLabel">Upload Image</h5>
-                                            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                                                <span aria-hidden="true">&times;</span>
-                                            </button>
-                                        </div>
-                                        <div className="modal-body">
-                                            ...
-                                        </div>
-                                        <div className="modal-footer">
-                                            <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-                                            <button type="button" className="btn btn-primary">Upload</button>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-
                             <div className="row">
                                 {this.state.images.map((image) => <div className="col-md-4 col-xs-12"><img className="img-thumbnail mt-1 mb-1" alt="" src={image}/></div>)}
                             </div>
