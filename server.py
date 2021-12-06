04adedd4b00c1803045ec9837dc8b147d4778f57
pje39613 <joss.whittle@rfi.ac.uk>
2021-12-06 13:44:17 +0000

    Add queued job

--- server.py
+++ server.py
@@ -2,7 +2,18 @@ from glob import glob
 import json
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory
 
+from rq import Queue
+from rq.job import Job
+from worker import conn
+
+q = Queue(connection=conn)
+
 app = Flask(__name__, static_url_path='')
+
+def process_upload(request):
+
+    with open('upload.txt', 'w') as fp:
+        fp.write(json.dumps(request))
 
 @app.route('/')
 def index():
@@ -16,9 +27,19 @@ def about():
 def serve_static(path):
     return send_from_directory('static', path)
 
-@app.route('/images')
+@app.route('/images', methods=['GET', 'POST'])
 def serve_images_index():
-    return json.dumps({ 'images': list(map(lambda path: f'/{path}', glob('images/*'))) })
+    if request.method == "GET":
+
+        return json.dumps({ 'images': list(map(lambda path: f'/{path}', glob('images/*'))) })
+
+    elif request.method == "POST":
+
+        print(request)
+
+        job = q.enqueue_call(func=process_upload, args=(request,), result_ttl=5000)
+        return json.dumps({ 'id': job.get_id() })
+
 
 @app.route('/images/<path:path>')
 def serve_images(path):
