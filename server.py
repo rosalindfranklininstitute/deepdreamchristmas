941f96d9597262209c731cd5c63412526dab6707
pje39613 <joss.whittle@rfi.ac.uk>
2021-12-06 06:50:43 +0000

    Make server serve images directory and index of images

--- server.py
+++ server.py
@@ -1,3 +1,5 @@
+from glob import glob
+import json
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory
 
 app = Flask(__name__, static_url_path='')
@@ -6,6 +8,18 @@ app = Flask(__name__, static_url_path='')
 def index():
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 @app.route('/static/<path:path>')
 def serve_static(path):
     return send_from_directory('static', path)
+
+@app.route('/images')
+def serve_images_index():
+    return json.dumps({ 'images': list(glob('images/*')) })
+
+@app.route('/images/<path:path>')
+def serve_images(path):
+    return send_from_directory('images', path)
