d2f813508d74cb4cda35e6f5d438af68832daae3
pje39613 <joss.whittle@rfi.ac.uk>
2021-12-06 05:53:29 +0000

    Add static files

--- server.py
+++ server.py
@@ -5,3 +5,7 @@ app = Flask(__name__, static_url_path='')
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/static/<path:path>')
+def serve_static(path):
+    return send_from_directory('static', path)
---
+++ static/app.js
@@ -0,0 +1,52 @@
+
+class HostApp extends React.Component {
+
+    state = {
+
+    };
+
+    componentDidMount() {
+
+
+
+    }
+
+    render() {
+
+        return (
+            <div className="fill">
+                <div className="d-flex h-100 text-center text-white bg-dark">
+                    <svg id="root-canvas" className="fill"/>
+                    <div id="root-ui" className="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+                        <header className="mb-auto">
+                            <div>
+                                <h3 className="float-md-start mb-0">RFI Christmas Dream</h3>
+                                <nav className="nav nav-masthead justify-content-center float-md-end">
+                                    <a className="nav-link active" aria-current="page" href="#">Upload</a>
+                                    <a className="nav-link" href="/about">About</a>
+                                </nav>
+                            </div>
+                        </header>
+                        <main id="device-link" className="px-3">
+                            <h1>RFI Christmas Dream</h1>
+                            <p className="lead">
+
+                            </p>
+                            <p className="lead">
+
+                            </p>
+                            <p>
+
+                            </p>
+                        </main>
+                        <footer className="mt-auto text-white-50">
+                            <p></p>
+                        </footer>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<HostApp />, document.getElementById('root'));
